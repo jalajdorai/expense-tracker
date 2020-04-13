4dbabff5376ce4aa29faab98cce4518375f41585
Jalaj <jalajdorai100@gmail.com>
2020-04-13 15:12:55 +0530

    Updates

---
+++ precache-manifest.f8c70a380cd788a68ace79e23f2abb1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b4853e080c1c3743e84d8bbcfb0630",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "aac0cf2fea8a06050243",
+    "url": "/expense-tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "f3fc6e8b22f8be9c985a",
+    "url": "/expense-tracker/static/js/2.e2c212e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.e2c212e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac0cf2fea8a06050243",
+    "url": "/expense-tracker/static/js/main.4dc60bdf.chunk.js"
+  },
+  {
+    "revision": "d76662676e30941a2687",
+    "url": "/expense-tracker/static/js/runtime-main.e45d9c66.js"
+  }
+]);
